f1181ec9b228158359131c7f027f36dd5ae1cd93
Mominjon <78495004+Mominjon@users.noreply.github.com>
2021-05-23 11:20:23 +0500

    the end

--- asseste/main.js
+++ asseste/main.js
@@ -32,24 +32,59 @@ const inputElement = document.querySelector(`.input`)
 const buttonpuskeElement = document.querySelector(`.button-1`)
 const removevideo = document.querySelector(`.remove-video`)
 const manzil = document.querySelector(`.box`)
-buttonpuskeElement.onclick = kattaMenu
+const hato = document.querySelector(`.hatolik`)
+const buttonKatta = document.querySelector(`.button-5`)
+const qoshimcha = document.querySelector(`.videolar-royhati`)
+const manzil2 = document.querySelector(`.qoshimcha-video`)
+const data = []
+const bilm = []
+const sinov = []
 
-function kattaMenu() {
+buttonpuskeElement.addEventListener(`click`, video6 => {
     removevideo.remove()
-    let x = 0
-    if (x == 0) {
+    hato.style.margin = `5px`
+    if (data.indexOf(inputElement.value) == -1) {
         let Nevvideo = document.createElement(`box`)
         Nevvideo.innerHTML = inputElement.value
-        inputElement.value = null
         manzil.prepend(Nevvideo)
-        Newvideo.remove()
-        x = 1
-    } else if (x == 1) {
-        let Newvideo = document.createElement(`box1`)
-        Newvideo.innerHTML = inputElement.value
-        inputElement.value = null
-        manzil.prepend(Newvideo)
-        Nevvideo.remove()
-        x = 0
+        data.push(inputElement.value)
+        Nevvideo.style.zIndex = `1` + 1
+    } else {
+        alert(`eror`)
     }
-}
+})
+buttonpuskeElement.addEventListener(`click`, qoshimcha23 => {
+
+    if (bilm.indexOf(inputElement.value) == -1) {
+        const Newvideo2 = document.createElement(`box1`)
+        Newvideo2.innerHTML = inputElement.value
+        qoshimcha.prepend(Newvideo2)
+        bilm.push(inputElement.value)
+    } else {
+
+    }
+})
+buttonpuskeElement.addEventListener(`click`, ohiri => {
+        if (sinov.indexOf(inputElement.value) == -1) {
+            const Newvideo3 = document.createElement(`box2`)
+            Newvideo3.innerHTML = inputElement.value
+            manzil2.appendChild(Newvideo3)
+            sinov.push(inputElement.value)
+        } else {
+
+        }
+    })
+    // function kattaMenu() {
+    //     removevideo.remove()
+    //     const Nevvideo = document.createElement(`box`)
+    //     Nevvideo.innerHTML = inputElement.value
+    //     inputElement.value = null
+    //     manzil.prepend(Nevvideo)
+    // }
+
+// function kichikmenu() {
+//     const Newvideo2 = document.createElement(`box1`)
+//     Newvideo2.innerHTML = inputElement.value
+//     inputElement.value = null
+//     qoshimcha.prepend(Newvideo2)
+// }
